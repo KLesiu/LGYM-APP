52ff9b98846e38d5be0ce08153a85a428bdb2902
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-10-30 21:24:49 +0100

    Add type do logout function

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 
 const Profile:React.FC=()=>{
     const [yourProfile,setYourProfile]=useState({name:localStorage.getItem('username'),email:localStorage.getItem('email'),rank:localStorage.getItem('rank')})
-    const logout=()=>{
+    const logout:VoidFunction=()=>{
         localStorage.removeItem('username')
         localStorage.removeItem('email')
         localStorage.removeItem('id')
