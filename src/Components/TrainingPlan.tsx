7ec4aae5ad81796b6a2440c78a5a4b1412dc566e
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-11-02 00:48:50 +0100

    Add createPlan section

--- src/Components/TrainingPlan.tsx
+++ src/Components/TrainingPlan.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
-
+import CreatePlan from './CreatePlan';
 import './styles/TrainingPlan.css'
 
 export interface Plan{
-    trainingDays: number;
+    days: PlanDay[]
+}
+
+export interface PlanDay{
+    trainingDay: string;
     exercises:Array<Exercise>
 }
 export interface Exercise{
@@ -15,13 +19,15 @@ const TrainingPlan:React.FC=()=>{
     const [plan,setPlan]=useState<Plan>()
     const [yourPlan,setYourPlan]=useState(<div id='withoutPlanContainer'>
         <h2>You dont have any plans!</h2>
-        <button>Create your plan now!</button>
+        <button onClick={()=>setIsPlanCreated(true)}>Create your plan now!</button>
     </div>)
+    const [namePlan,setNamePlan]=useState<string>()
+    const [isPlanCreated,setIsPlanCreated]=useState<boolean>(false)
     
     return(
-        <section>
+        <section id='trainingPlanSection'>
             {yourPlan}
-            
+            {isPlanCreated?<CreatePlan/>:''}
         </section>
     )
 }
