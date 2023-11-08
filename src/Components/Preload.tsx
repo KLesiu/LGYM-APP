12f1e75d9924f6f650c7ac54d47c9e374e2fc9fc
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-11-08 19:49:38 +0100

    Change preload animation, fn handleChange is going to do after 500ms since page load

--- src/Components/Preload.tsx
+++ src/Components/Preload.tsx
@@ -5,11 +5,15 @@ const Preload:React.FC=()=>{
     const [quote,setQuote]=useState<boolean>(false)
     const handleChange:VoidFunction=():void=>setQuote(true)
     useEffect(()=>{
+        setTimeout(handleChange,500)
+    },[])
+    useEffect(()=>{
         if(quote===true){
             document.querySelector(".icon-dumbell")?.classList.add("rotate")
             document.querySelector("#quote")?.classList.add("appearance")
         } 
         },[quote])
+
     return(
         <div id='preLoadDiv'>
             <div id='preLoadContainer'>
@@ -20,7 +24,7 @@ const Preload:React.FC=()=>{
                     <a href='/register'><span>REGISTER</span></a>
                 </div>
                 <div id='quoteContainer'>
-                <span onClick={handleChange} className="icon-dumbell material-symbols-outlined">
+                <span  className="icon-dumbell material-symbols-outlined">
                 exercise
                 </span>
                 <p id='quote'>{quote===true?`'Strength does not come from winning. Your struggles develop your strengths. When you go through hardships and decide not to surrender, that is strength. When you make an impasse passable, that is strength. But you must have ego, the kind of ego which makes you think of yourself in terms of superlatives. You must want to be the greatest. We are all starved for compliments. So we do things that get positive feedback.' (Arnold Schwarzenegger, 1982)`:""}</p>
