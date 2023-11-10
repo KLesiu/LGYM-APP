d2f1b9a87794f37b76fc09d3c01ce38c843768e5
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-11-10 01:11:10 +0100

    Add loading after login before home section

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -3,12 +3,16 @@ import Menu from './Menu'
 import TrainingPlan from './TrainingPlan'
 import {  useState,useEffect } from 'react'
 import LoadingSection from './LoadingSection'
+import Loading from './Loading'
 import logo from './img/logo300.png'
 
 
 const Home:React.FC=()=>{
     const [view,setView]=useState<JSX.Element>(<TrainingPlan/>)
     const [sectionLoading,setSectionLoading]=useState<boolean>(true)
+    const [mainSectionLoading,setMainSectionLoading]=useState<boolean>(true)
+    const [componentLoaded,setComponentLoaded]=useState<boolean>(false)
+    const offLoading:VoidFunction=():void=>setComponentLoaded(true)
     const changeView=(view:JSX.Element):void=>{
         setTimeout(()=>setSectionLoading(false),1100)
         setView(view)
@@ -24,6 +28,8 @@ const Home:React.FC=()=>{
             <span id='holderForLogo'><img className='logoOfHome' src={logo} alt="" /></span>
             {sectionLoading?<LoadingSection/>:''}
             {view}
+            {componentLoaded?'':<Loading offLoading={offLoading} />}
+            
             <Menu viewChange={changeView}  />
         </main>
     )
