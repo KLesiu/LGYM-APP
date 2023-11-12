cb6631cd405cffd370fa58413e9f0bd20256255e
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-11-12 16:14:15 +0100

    Style currentTrainingHistorySession

--- src/Components/CurrentTrainingHistorySession.tsx
+++ src/Components/CurrentTrainingHistorySession.tsx
@@ -6,27 +6,39 @@ import ExerciseTraining from "./interfaces/ExerciseTrainingInterface"
 const CurrentTrainingHistorySession:React.FC<CurrentTrainingHistorySessionProps>=(props)=>{
     const [infoAboutSession,setInfoAboutSession]=useState<JSX.Element>()
     const [exercises,setExercises]=useState<ExerciseTraining[]>()
+    const [currentExercisesNumberAtPage,setCurrentExercisesNumberAtPage]=useState<number>(0)
     const getInformationAboutSession=async():Promise<void>=>{
         const response:string|Training= await props.getInformationAboutHistorySession(props.id).then(res=>res)
         if(typeof response !== 'string'){
             setExercises(response.exercises)
             setInfoAboutSession(()=><div className="sessionTrainingContainer">
                 <h3>TrainingDay: {response.type}</h3>
-                
+                <div><span>Rep</span><span>Weight</span></div>
             </div>)
         }
     }
+    const showCurrentExercises:VoidFunction=():void=>{
+
+    }
+    const showFirstExercises:VoidFunction=():void=>{
+        const divs = document.querySelectorAll('.exerciseDiv')
+        divs.forEach((ele,index)=>index<14?ele.classList.remove('hidden'):'')
+        setCurrentExercisesNumberAtPage(14)
+    }
     useEffect(()=>{
      getInformationAboutSession()
+     if(exercises) showFirstExercises()
+     
     },[])
+  
     return(
         <div id="currentTrainingHistorySession">
             <h2>SessionId: {props.id}</h2>
             <p>Date: {props.date}</p>
             {infoAboutSession}
-            {exercises?.map((ele,index)=><div key={index} className="exerciseDiv">
+            {exercises?.map((ele,index)=><div key={index} className="exerciseDiv bottomBorderP hidden">
                 
-                <span>{index%2?<p className="bottomBorderP">{ele.field}</p>:<p>{ele.field}</p>} : {index%2?<p className="bottomBorderP">{ele.score}kg</p>:<p>{ele.score}</p>}</span>
+                <span>{index%2?<p className="hidden">{ele.field}</p>:<p>{ele.field.slice(0,ele.field.length-3)}</p>}  {index%2?<span >{ele.score}kg</span>:<span className="rightBorder">{ele.score}</span>}</span>
             </div>)}
         </div>
 
