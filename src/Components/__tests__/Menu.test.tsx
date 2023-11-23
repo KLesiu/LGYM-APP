87193de9316191c4e02c039b857636e4951a647b
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-11-23 22:25:36 +0100

    Add menu tests

---
+++ src/Components/__tests__/Menu.test.tsx
@@ -0,0 +1,61 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import Menu from '../Menu';
+
+test('renders Menu component', () => {
+  render(<Menu viewChange={()=>{}} />);
+  const menuNav = screen.getByRole('navigation');
+  expect(menuNav).toBeInTheDocument();
+
+  const buttons = screen.getAllByRole('button');
+  expect(buttons).toHaveLength(5);
+});
+
+test('changes view when clicking on "note" button', () => {
+  const viewChangeMock = jest.fn();
+  render(<Menu viewChange={viewChangeMock} />);
+  
+  const noteButton = screen.getByText('note');
+  fireEvent.click(noteButton);
+
+  expect(viewChangeMock).toHaveBeenCalledWith(expect.any(Object));
+});
+test('changes view when clicking on "calendar_month" button', () => {
+    const viewChangeMock = jest.fn();
+    render(<Menu viewChange={viewChangeMock} />);
+    
+    const noteButton = screen.getByText('calendar_month');
+    fireEvent.click(noteButton);
+  
+    expect(viewChangeMock).toHaveBeenCalledWith(expect.any(Object));
+  });
+test('changes view when clicking on "add_box" button', () => {
+    const viewChangeMock = jest.fn();
+    render(<Menu viewChange={viewChangeMock} />);
+    
+    const noteButton = screen.getByText("add_box");
+    fireEvent.click(noteButton);
+  
+    expect(viewChangeMock).toHaveBeenCalledWith(expect.any(Object));
+  });
+test('changes view when clicking on "trophy" button', () => {
+    const viewChangeMock = jest.fn();
+    render(<Menu viewChange={viewChangeMock} />);
+    
+    const noteButton = screen.getByText("trophy");
+    fireEvent.click(noteButton);
+  
+    expect(viewChangeMock).toHaveBeenCalledWith(expect.any(Object));
+  });
+
+test('changes view when clicking on "account_circle" button', () => {
+    const viewChangeMock = jest.fn();
+    render(<Menu viewChange={viewChangeMock} />);
+    
+    const noteButton = screen.getByText("account_circle");
+    fireEvent.click(noteButton);
+  
+    expect(viewChangeMock).toHaveBeenCalledWith(expect.any(Object));
+  });
+
+
+  
